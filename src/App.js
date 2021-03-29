6cd8e43c231a94050e94154c50a651036f8d6ae9
Niraj Kamdar <nirajkamdar27@pm.me>
2021-03-29 17:30:05 +0530

    Added signup and signin - blockchain integration

--- src/App.js
+++ src/App.js
@@ -2,18 +2,50 @@ import React, {useState} from 'react';
 import {Box, Grid, useColorModeValue,} from '@chakra-ui/react';
 import {ColorModeSwitcher} from './ColorModeSwitcher';
 import Router from "./components/router";
+import {useAsyncMemo} from "use-async-memo";
+import {getHub, getMetamask} from "./components/utils";
+import {createNotification} from "./components/notification";
+import {ErrorBoundary} from "react-error-boundary";
+import ErrorFallback from "./components/error";
 
+export const IdentityContext = React.createContext(null)
+export const MetamaskContext = React.createContext(null)
+export const HubContext = React.createContext(null)
 export const UserContext = React.createContext(null)
 
 function App() {
     const [identity, setIdentity] = useState(null)
+    const [user, setUser] = useState(null)
+    const metamask = useAsyncMemo(async() => {
+        try{
+            return await getMetamask()
+        } catch(err) {
+            createNotification("error", err.name, err.message)
+        }
+    }, [], {})
+    const hub = useAsyncMemo(async () => {
+        try{
+            return await getHub(identity)
+        } catch(err) {
+            createNotification("error", err.name, err.message)
+        }
+    }, [identity], {})
+
     return (
         <Box textAlign="center" fontSize="xl" bg={useColorModeValue('gray.50', 'gray.800')}>
             <Grid minH="100vh" p={3}>
                 <ColorModeSwitcher justifySelf="flex-end"/>
-                <UserContext.Provider value={{identity, setIdentity}}>
-                    <Router/>
-                </UserContext.Provider>
+                <IdentityContext.Provider value={{identity, setIdentity}}>
+                    <MetamaskContext.Provider value={metamask}>
+                        <HubContext.Provider value={hub}>
+                            <UserContext.Provider value={{user, setUser}}>
+                                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                                    <Router/>
+                                </ErrorBoundary>
+                            </UserContext.Provider>
+                        </HubContext.Provider>
+                    </MetamaskContext.Provider>
+                </IdentityContext.Provider>
             </Grid>
         </Box>
     );
--- src/contracts/accounts.js
+++ src/contracts/accounts.js
@@ -1,15 +1,16 @@
 import {Contract} from 'ethers'
 
-const gptAddress = ""
+const gptAddress = "0xB4341D98e12B6e93dcf231dd615B16d8b208C83c"
 const gptABI = [
     "function signUp(string memory accountURI, uint256 accountID) external returns (uint256)",
-    "function signIn(uint256 accountID) external view returns (bool)",
+    "function signIn(uint256 accountID) external view returns (string memory)",
     "function deleteAccount(uint accountID) external",
     "function tokenURI(uint256 tokenId) public view virtual override returns (string memory)",
-    "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)"
+    "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+    "function ownerOf(uint256 tokenId) public view virtual returns (address)"
 ]
 
-export function getContract(provider, signer){
+export function getGptContract(provider, signer){
     const gptContract = new Contract(gptAddress, gptABI, provider)
     const gptSigner = gptContract.connect(signer)
     return {gptContract, gptSigner}
